3fa76eaeb7b18edf34c60811eab8aa9b8ba63d15
Omkar Yadav <omkaryad.101@gmail.com>
2024-10-03 15:20:04 +0530

    first commit

--- kars/views.py
+++ kars/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Kar
 
-
+# Home view to display the list of cars
 def home(request):
-    kars= Kar.objects.all()
-    return render(request, 'home.html', {'kars': kars})
+    cars = Kar.objects.all()
+    return render(request, 'home.html', {'cars': cars})
 
+# Detail view to display a specific car's details
 def car_detail(request, car_id):
     car = get_object_or_404(Kar, pk=car_id)
     return render(request, 'car_detail.html', {'car': car})
-
-# Create your views here.
--- kars_website/urls.py
+++ kars_website/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from kars import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('kars.urls')),
+    path('car/<int:car_id>/', views.car_detail, name='car_detail'),
 ]
 
 # Add this to serve media files in development
